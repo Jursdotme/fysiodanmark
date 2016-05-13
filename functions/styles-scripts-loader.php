f9296aada7d0a307727f5b0e2854b59fca2f3a61
Rasmus Jürs <rasmus@jurs.me>
2016-05-13 11:35:59 +0200

    Move query to header

--- functions/styles-scripts-loader.php
+++ functions/styles-scripts-loader.php
@@ -10,10 +10,10 @@ function secondthought_header_scripts()
       wp_deregister_script( 'jquery' ); // Deregister WordPress jQuery
 
       if (current_user_can('activate_plugins')) {
-        wp_register_script('jquery', get_template_directory_uri() . '/javascripts/lib/jquery-2.1.4.min.js', array(), '1.0.0', true); // Custom scripts
+        wp_register_script('jquery', get_template_directory_uri() . '/javascripts/lib/jquery-2.1.4.min.js', array(), '1.0.0', false); // Custom scripts
         wp_enqueue_script('jquery'); // Enqueue it!
       } else {
-        wp_register_script('jquery', "http" . ($_SERVER['SERVER_PORT'] == 443 ? "s" : "") . "://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js", array(), '2.1.4', true); // jQuery
+        wp_register_script('jquery', "http" . ($_SERVER['SERVER_PORT'] == 443 ? "s" : "") . "://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js", array(), '2.1.4', false); // jQuery
         wp_enqueue_script('jquery'); // Enqueue it!
       }
 
@@ -21,7 +21,7 @@ function secondthought_header_scripts()
       wp_enqueue_script('modernizr'); // Enqueue it!
 
 
-        wp_register_script('secondthoughtscripts', get_template_directory_uri() . '/build/scripts/all.min.js', array('jquery'), '1.0.0', true); // Custom scripts
+        wp_register_script('secondthoughtscripts', get_template_directory_uri() . '/build/scripts/all.min.js', 'jquery', '1.0.0', true); // Custom scripts
         wp_enqueue_script('secondthoughtscripts'); // Enqueue it!
     }
 }
