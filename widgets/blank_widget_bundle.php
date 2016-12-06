07d5240b45370d211959c3606dabff925398101e
Rasmus Jürs <rasmus@jurs.me>
2016-12-06 12:45:32 +0100

    Make paths child theme compatible

--- widgets/blank_widget_bundle.php
+++ widgets/blank_widget_bundle.php
@@ -1,13 +1,13 @@
 <?php
-function add_my_awesome_widgets_collection($folders){
-	$folders[] = get_stylesheet_directory().'/widgets/';
+function fysiodanmark_widgets_collection($folders){
+	$folders[] = realpath(__DIR__ . '/..') . '/widgets/';
 	return $folders;
 }
-add_filter('siteorigin_widgets_widget_folders', 'add_my_awesome_widgets_collection');
+add_filter('siteorigin_widgets_widget_folders', 'fysiodanmark_widgets_collection');
 
 function fd_feature_template_file( $filename, $instance, $widget ){
 	// And this one for themes
-	$filename = get_stylesheet_directory() . '/widgets/secondthought-feature-widget/secondthought-feature-widget-template.php';
+	$filename = realpath(__DIR__ . '/..') . '/widgets/secondthought-feature-widget/secondthought-feature-widget-template.php';
   return $filename;
 }
 add_filter( 'siteorigin_widgets_template_file_secondthought-feature-widget', 'fd_feature_template_file', 10, 3 );
