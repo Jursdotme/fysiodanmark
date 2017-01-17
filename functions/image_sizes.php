1ba69fd0858c3c7335a7d198572be3b8d8cbd922
Rasmus Jürs <rasmus@jurs.me>
2017-01-17 09:42:35 +0100

    add `acf` check to image sizes

--- functions/image_sizes.php
+++ functions/image_sizes.php
@@ -9,7 +9,11 @@ if (function_exists('add_theme_support'))
     add_theme_support('post-thumbnails');
     // add_image_size('large', 700, '', true); // Large Thumbnail
     // add_image_size('medium', 250, '', true); // Medium Thumbnail
-    add_image_size('portrait', get_field('portrait_width','option'), get_field('portrait_height','option'), true); // Small Thumbnail
+
+    if (function_exists('get_field')) {
+      add_image_size('portrait', get_field('portrait_width','option'), get_field('portrait_height','option'), true); // Small Thumbnail
+    }
+    
     add_image_size('secondthought_feature', 400, 200, true); // Custom Thumbnail Size call using the_post_thumbnail('custom-size');
 
     // Enables post and comment RSS feed links to head
